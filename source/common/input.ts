088f5e83c80ec4ed4a32e9296e9d4863032a3e8e
Kyle Mayes <kyle@mayeses.com>
2019-12-13 21:52:51 -0500

    Add input common code

---
+++ source/common/input.ts
@@ -0,0 +1,31 @@
+import * as fs from "fs";
+import * as path from "path";
+
+/** Reads the supplied input file. */
+export function read(input: string): string {
+  const p = path.resolve(__dirname, "../../input", input);
+  return fs
+    .readFileSync(p)
+    .toString("utf8")
+    .trim();
+}
+
+/** Reads the supplied input file and gets the non-empty comma-separated values. */
+export function csv(input: string): string[] {
+  return read(input)
+    .split(",")
+    .filter(l => l.trim() !== "");
+}
+
+/** Reads the supplied input file and gets the non-empty lines. */
+export function lines(input: string): string[] {
+  return read(input)
+    .split("\n")
+    .map(l => l.trim())
+    .filter(l => l !== "");
+}
+
+/** Reads the supplied input file and gets the non-empty comma-separated values for each line. */
+export function linesCsv(input: string): string[][] {
+  return lines(input).map(l => l.split(",").filter(l => l.trim() !== ""));
+}
